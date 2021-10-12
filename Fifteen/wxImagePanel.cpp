af2ecdd7bd0cf7f0c8ec19c3f8b27bb63a37c4a5
n0f4ph4mst3r <vladredsoup@gmail.com>
2021-10-12 22:33:46 +0300

    feat(image-panel): add animation for blocks

--- Fifteen/wxImagePanel.cpp
+++ Fifteen/wxImagePanel.cpp
@@ -13,8 +13,8 @@ wxImagePanel::wxImagePanel(wxFrame* parent, wxString file, wxBitmapType format) 
 
     n = 1;
 
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
         {
             blocks.push_back(source.GetSubBitmap(wxRect(w * i, w * j, w, w)));
             grid[i][j] = n;
@@ -39,32 +39,82 @@ void wxImagePanel::mouseDown(wxMouseEvent& event)
 
     if (dx != 0 || dy != 0)
     {
+        x1 = x;
+        x2 = x + dx;
+        y1 = y;
+        y2 = y + dy;
+
+        x_pos = x * w;
+        y_pos = y * w;
+
+        this->dx = dx;
+        this->dy = dy;
+
     n = grid[x][y];
-    grid[x][y] = 16;
-    grid[x + dx][y + dy] = n;
     renderTimer.Start(10);
     }
 }
 
 void wxImagePanel::paintEvent(wxPaintEvent& evt)
 {
-    wxPaintDC dc(this);
+    wxBufferedPaintDC dc(this);
     render(dc);
 }
 
 void wxImagePanel::render(wxDC& dc)
 {
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++) 
+    static bool IsFirstRender = true;
+    if (IsFirstRender)
+    {
+        dc.SetBackground(*wxWHITE_BRUSH);
+        dc.DrawBitmap(source, 0, 0, false);
+        IsFirstRender = false;
+    }
+    else
+    {
+        int speed_x = (x2 - x1) * 25;
+        int speed_y = (y2 - y1) * 25;
+
+        int k = grid[x2][y2];
+
+        dc.DrawBitmap(blocks[k - 1], x_pos, y_pos, false);
+
+        x_pos += speed_x;
+        if ((x_pos > x2 * w && dx == 1) || (x_pos < x2*w && dx == -1))
         {
-            n = grid[i][j] - 1;
-            dc.DrawBitmap(blocks[n], w*i, w*j, false);
+            x_pos = x2 * w;
         }
-    renderTimer.Stop();
+
+        y_pos += speed_y;
+        if ((y_pos > y2 * w && dy == 1) || (y_pos < y2*w && dy == -1))
+        {
+            y_pos = y2 * w;
+        }
+
+        dc.DrawBitmap(blocks[n-1], x_pos, y_pos, false);
+
+        if (x_pos == x2*w && y_pos == y2*w)
+        {
+            grid[x1][y1] = 16;
+            grid[x2][y2] = n;
+            renderTimer.Stop();
+        }
+    }
 }
 
 void wxImagePanel::RenderTimer(wxTimerEvent& event)
 {
-    wxClientDC dc(this);
+    wxClientDC dc_Client(this);
+    wxClientDC* Client;
+    Client = &dc_Client;
+    wxBufferedDC dc(Client, wxNullBitmap, wxBUFFER_CLIENT_AREA);
     render(dc);
 }
+ /*
+ for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+            {
+                n = grid[i][j] - 1;
+                dc.DrawBitmap(blocks[n], w * i, w * j, false);
+            }
+ */

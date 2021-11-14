d51c31098baad8cce6261f2fcfe80cafaba6ed5f
n0f4ph4mst3r <vladredsoup@gmail.com>
2021-11-15 01:23:16 +0300

    refactor(image-panel): add movement logic for block in render-timer method

--- Fifteen/fifteen.h
+++ Fifteen/fifteen.h
@@ -1,9 +1,21 @@
 #pragma once
-#include "Block.h"
+#include <wx/wx.h>
+#include <wx/sizer.h>
 #include <vector>
 #include <algorithm>
 #include <random>
 #include <chrono>
+
+//interface for block of puzzle
+struct Block
+{
+	int id; //block number
+	int positionX, positionY; //actual block position
+	wxBitmap bmp; //block picture, using to draw on panel
+	wxPoint offset;
+	Block(wxBitmap bmp, int id, int x = 0, int y = 0) : bmp(bmp), id(id), positionX(x), positionY(y), offset(wxPoint()) {}
+};
+
 //interface for puzzle
 //instance of class is a logical representation of the puzzle
 class Fifteen
@@ -18,5 +30,6 @@ public:
 	std::vector<std::vector <Block*>> Grid();
 	int Size();
 
-	void Swap(Block* replaceable, Block* substitute); 
+	void Swap(Block* block1, Block* block2); 
 };
+
